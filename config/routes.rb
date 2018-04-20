8af9e58c3fe0c19042d0ee164a4c01ab4ef821c2
Michael Lamb <mflambjr@gmail.com>
2018-04-20 17:17:06 -0400

    create users controller, create boats model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/create'
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'boats/index'
+  get 'boats/new'
+  get 'boats/create'
+  get 'boats/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_19_144215) do
+ActiveRecord::Schema.define(version: 2018_04_20_203542) do
+
+  create_table "boats", force: :cascade do |t|
+    t.string "name"
+    t.integer "capacity"
+    t.string "location"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.index ["user_id"], name: "index_boats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
